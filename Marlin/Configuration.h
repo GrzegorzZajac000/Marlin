dfc4cc6ca501380f4c3f5b00ba718f3f24b35d15
Grzegorz Zając <grzegorz.zajac000@gmail.com>
2020-08-20 12:04:59 +0200

    Probe Z Offset FIX, LIN_ADNAVCE K VALUE FIX

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -997,7 +997,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -48, 7, -0.8 }
+#define NOZZLE_TO_PROBE_OFFSET { -48, 7, -0.5 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
